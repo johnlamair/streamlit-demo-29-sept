f1c06eac82f7fce8cc69942b27db32e0c79ab017
John <156053292+johnlamair@users.noreply.github.com>
2025-10-15 12:11:45 +0000

    updated everything

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("This is the app title")
+st.header('This is the app header')
+st.markdown("### this is the app markdown")
+
+st.write("Hello world")
+
+## filters and buttons
+st.checkbox("yes")
+st.button("click")
+st.radio("Pick your sport", ["basketball", "football"])
+st.selectbox("Pick your sport", ["basketball", "football"])
+st.multiselect("Pick your sport", ["basketball", "football"])
+
+## sidebar
+st.sidebar.title("this is my sidebar")
+st.sidebar.button("click")
+st.sidebar.radio("pick your color", ["red", "blue"])
+
+## data viz (2 charts)
+df = pd.DataFrame(np.random.randn(10,2), columns=["x", "y"])
+st.line_chart(df)
